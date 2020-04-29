0680231edda90f7d577fd2404846e5c618ba6262
alex <info@alex-relov.ru>
2020-04-29 12:51:59 +0500

    Add project files.

---
+++ CollectionViewBug/CollectionViewBug/Models/ItemModel.cs
@@ -0,0 +1,14 @@
+﻿namespace CollectionViewBug.Models
+{
+    public class ItemModel
+    {
+        public string Name { get; set; }
+        public string Group { get; set; }
+
+        public ItemModel(string group, string name)
+        {
+            Name = name;
+            Group = group;
+        }
+    }
+}
---
+++ CollectionViewBug/CollectionViewBug/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,47 @@
+﻿using CollectionViewBug.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace CollectionViewBug.ViewModels
+{
+    public class MainPageViewModel
+    {
+        public ObservableCollection<Grouping<string, ItemModel>> Items { get; set; } = new ObservableCollection<Grouping<string, ItemModel>>();
+
+        ItemModel[] DataList = new ItemModel[]
+        {
+            new ItemModel("A","One"),
+            new ItemModel("A","Two"),
+            new ItemModel("A","Three"),
+            new ItemModel("B","Four"),
+        };
+
+        public ICommand UpdateItems { get; }
+
+        public MainPageViewModel()
+        {
+            InsertData();
+            UpdateItems = new Command(() =>
+            {
+                Items.Clear();
+                InsertData();
+            });
+        }
+
+        void InsertData()
+        {
+            var grouped = DataList.GroupBy(g => g.Group);
+
+            foreach (var group in grouped)
+            {
+                Items.Add(new Grouping<string, ItemModel>(group.Key,group));
+            }
+        }
+
+    }
+}
